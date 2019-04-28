02e7d3f674d7928914dbd37a152ddd23b7343465
danielorbach <ddorbach@gmail.com>
2019-04-28 19:53:17 +0300

    Add homework exercises
    Add three homework exercises:
    1. practicepython.org/exercise/2014/04/30/13-fibonacci.html
    2. practicepython.org/exercise/2014/05/28/16-password-generator.html
    3. developers.google.com/edu/python/exercises/log-puzzle

---
+++ lesson-1/homework/hw2/main.py
@@ -0,0 +1,9 @@
+"""
+Homework 2: Password Generator
+Link: www.practicepython.org/exercise/2014/05/28/16-password-generator.html
+
+Write a password generator in Python.
+Be creative with how you generate passwords - strong passwords have a mix of lowercase letters, uppercase letters, numbers, and symbols.
+The passwords should be random, generating a new password every time the user asks for a new password.
+Include your run-time code in a main method.
+"""
---
+++ lesson-1/homework/hw3/main.py
@@ -0,0 +1,9 @@
+"""
+Homework 2: Log Puzzle
+Link: developers.google.com/edu/python/exercises/log-puzzle
+
+An image of an animal has been broken it into many narrow vertical stripe images.
+The stripe images are on the internet somewhere, each with its own url.
+The urls are hidden in a web server log file.
+Your mission is to find the urls and download all image stripes to re-create the original image.
+"""
